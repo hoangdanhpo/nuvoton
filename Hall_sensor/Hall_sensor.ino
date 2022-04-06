3b17ebc4fe8c4c9e3ee6766276afdbfa426e74b5
danh <hoangdanhpo@gmail.com>
2022-04-06 21:02:35 +0700

    Update

---
+++ Hall_sensor/Hall_sensor.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+   DDRB=B00000111;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  PORTB = B00000101; delay(10); // state 1
+  PORTB = B00000001; delay(10); // state 2
+  PORTB = B00000011; delay(10); // state 3
+  PORTB = B00000010; delay(10); // state 4
+  PORTB = B00000110; delay(10); // state 5
+  PORTB = B00000100; delay(10); // state 6
+}
